529db4a0960e2e166c5cfa0b708b21ff91729f3d
Katy Duncan <duncan.katya@gmail.com>
2020-05-20 14:54:00 -0700

    add http requests

--- first-app/logger.js
+++ first-app/logger.js
@@ -1,17 +1,32 @@
-var x =;
+const EventEmitter = require('events');
+// const emitter = new EventEmitter();
 
 var url = 'http://mylogger.io/log';
 
-function log(message) {
-// Send an HTTP request
-console.log(message);
-
-
-
+// JS CLASS 
+class Logger extends EventEmitter {
+    log(message) {
+        // Send an HTTP request
+        console.log(message);
+        
+        //Raise an event where 'this' is now the parent Emitter class
+        this.emit('messageLogged', { id: 1, url: 'http://' });
+        }
 }
+
+module.exports = Logger;
+
+// function log(message) {
+// // Send an HTTP request
+// console.log(message);
+
+
+// //Raise an event
+// emitter.emit('messageLogged', { id: 1, url: 'http://' });
+// }
 
 // module.exports.log = log;
 // module.exports.endPoint = url;
 
-module.exports = log;
+// module.exports = log;
 
