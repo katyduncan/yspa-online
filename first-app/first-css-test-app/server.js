ea2fda79b2ec4de0d04668422a2be98e93325958
Katy Duncan <duncan.katya@gmail.com>
2020-05-26 23:32:40 -0700

    start building node app to capture yspa data

--- first-app/first-css-test-app/server.js
+++ first-app/first-css-test-app/server.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 
 // Setting up the public directory
+// You can use a response to send a single HTML file (see landing-page.js)
+// and a public directory to host other static assets such as CSS files, JS scripts, and images.
 app.use('/public', express.static('landing-page'));
 
 app.listen(port, () => console.log(`Listening on port ${port}...`));
